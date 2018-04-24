5f9dac0324718b3ed335df8fb3dd94552bc05e98
Scion01 <nishantnareshtyagi@gmail.com>
2018-04-24 13:34:44 +0530

    This is with add a new Query

---
+++ app/src/main/java/com/example/hauntarl/smartfarming/AddingNewQuery.java
@@ -0,0 +1,17 @@
+package com.example.hauntarl.smartfarming;
+
+public class AddingNewQuery {
+    String desc;
+    String subject;
+    String imageUrl;
+    String solved;
+    String solution;
+
+    public AddingNewQuery(String desc, String subject, String imageUrl, String solved, String solution) {
+        this.desc = desc;
+        this.subject = subject;
+        this.imageUrl = imageUrl;
+        this.solved = solved;
+        this.solution = solution;
+    }
+}
